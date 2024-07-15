a75a0c4ec14b5552a3c0684dd8df2ce24d765142
Pedro Navarrete <navhpedro@gmail.com>
2024-07-15 18:51:12 -0400

    Mobile Results

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 
-import colors from '../styles/styles';
-
 import Steps from '../components/Steps';
-import { NavButton } from '../components/Buttons';
 import Product from '../components/Product';
+import { NavButton } from '../components/Buttons';
+
+import colors from '../styles/styles';
 
 const ResultsContainer = styled.div`
   display: flex;
@@ -23,17 +23,21 @@ const ResultsContainer = styled.div`
 
 const Title = styled.h1`
   font-family: 'Roboto Condensed', sans-serif;
-  font-weight: 300;
+  font-weight: 400;
   font-size: 48px;
   line-height: 1.1;
   letter-spacing: 0.06em;
   text-align: center;
   color: ${colors.VerveBlack};
+
+  @media (max-width: 768px) {
+    font-size: 26px;
+  }
 `;
 
 const Subtitle = styled.h2`
   font-family: 'Roboto', sans-serif;
-  font-weight: 300;
+  font-weight: 400;
   font-size: 14.5px;
   line-height: 1.5;
   letter-spacing: 0;
@@ -42,6 +46,10 @@ const Subtitle = styled.h2`
   text-align: center;
   margin-top: 10px;
   margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
 
 const ButtonsContainer = styled.div`
