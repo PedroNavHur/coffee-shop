51c7930e005c7431ba37e3746e0a2304a4df9db1
Pedro Navarrete <navhpedro@gmail.com>
2024-07-15 08:21:23 -0400

    Finished Desktop NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../styles/styles';
+
+const Nav = styled.nav`
+  background-color: ${colors.VerveBlack};
+  color: white;
+  width: 100%;
+  height: 165.2px;
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  padding: 0 63.5px;
+`;
+
+const NavLogo = styled.a`
+  font-family: 'Roboto Condensed', sans-serif;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 48px;
+  text-decoration: none;
+  text-align: center;
+  color: white;
+`;
+
+const Placeholder = styled.div`
+  width: 46px;
+`;
+
+const MenuIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 27px;
+  cursor: pointer;
+`;
+
+const Bar = styled.div`
+  width: 46px;
+  height: 3px;
+  border-radius: 3px;
+  background-color: white;
+`;
+
+const NavBar = () => {
+  return (
+    <Nav>
+      <NavContainer>
+        <Placeholder />
+        <NavLogo href="/">The Coffee Shop</NavLogo>
+        <MenuIcon>
+          <Bar />
+          <Bar />
+          <Bar />
+        </MenuIcon>
+      </NavContainer>
+    </Nav>
+  );
+}
+
+export default NavBar;
