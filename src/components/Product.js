41af47d6c8c96e16982d2a3474395eb7566944b5
Pedro Navarrete <navhpedro@gmail.com>
2024-07-15 20:05:54 -0400

    Product for Mobile

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,20 +11,42 @@ import HorizontalImage from '../assets/banners/horizontal.png';
 import VerticalImage from '../assets/banners/vertical.png';
 import LongVerticalImage from '../assets/banners/long-vertical.png';
 
-const HorizontalBar = styled.img`
+const DesktopHorizontalBar = styled.img`
   width: 812px;
   height: 2px;
   padding: 30px 0 0 0;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
-const VerticalBar = styled.img`
+const DesktopVerticalBar = styled.img`
   width: 2px;
   height: 274px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
-const LongVerticalBar = styled.img`
+const DesktopLongVerticalBar = styled.img`
   width: 2px;
-  height: 464px;
+  height: auto;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileHorizontalBar = styled.img`
+  width: calc(100vw - 48px);
+  height: 2px;
+  object-fit: cover;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
 `;
 
 const ProductContainer = styled.div`
@@ -34,6 +56,12 @@ const ProductContainer = styled.div`
   justify-content: center;
   width: 1200px;
   background-color: rgba(34, 34, 34, 0.05);
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: calc(100vw - 48px);
+    margin: 24px;
+  }
 `;
 
 const BestMatchContainer = styled.div`
@@ -48,12 +76,21 @@ const SpecsContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    padding: 0;
+    margin: 0;
+  }
 `;
 
 const BestMatchImage = styled.img`
   width: 313px;
   height: auto;
   margin-bottom: 10px;
+
+  @media (max-width: 768px) {
+    width: 282px;
+  }
 `;
 
 const BestMatchTitle = styled.h3`
@@ -74,6 +111,10 @@ const BestMatchPrice = styled.p`
   margin: 0;
   padding: 0;
   color: ${colors.VerveBlack};
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
 const DetailsContainer = styled.div`
@@ -81,8 +122,15 @@ const DetailsContainer = styled.div`
   flex-direction: row;
   width: 100%;
   justify-content: space-between;
+  align-items: center;
   padding: 0 20px;
   margin: 0 20px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0;
+    margin: 0;
+  }
 `;
 
 const Detail = styled.div`
@@ -99,6 +147,22 @@ const Detail = styled.div`
     height: auto;
     margin: 20px;
   }
+
+  @media (max-width: 768px) {
+    margin: 0;
+    padding: 16px;
+    width: calc(100vw - 48px);
+    flex-direction: column;
+  }
+`;
+
+const DetailContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
 `;
 
 const DetailTitle = styled.h4`
@@ -122,6 +186,12 @@ const DetailText = styled.p`
   padding: 0 12px;
   white-space: pre-line;
   color: ${colors.VerveBlack};
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    padding: 24px;
+    margin: 0;
+  }
 `;
 
 const MediumText = styled.p`
@@ -133,6 +203,12 @@ const MediumText = styled.p`
   text-align: center;
   text-transform: uppercase;
   color: ${colors.VerveBlack};
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    padding: 24px;
+    margin: 0;
+  }
 `;
 
 const Product = () => {
@@ -145,47 +221,60 @@ const Product = () => {
         <BestMatchPrice>$20.50</BestMatchPrice>
       </BestMatchContainer>
 
-      <LongVerticalBar src={LongVerticalImage} alt="Long Vertical Bar" />
+      <DesktopLongVerticalBar src={LongVerticalImage} alt="Long Vertical Bar" />
+      <MobileHorizontalBar src={HorizontalImage} alt="Mobile Horizontal Bar" />
 
       <SpecsContainer>
         <Detail>
           <DetailTitle>WHY YOU'LL LOVE IT</DetailTitle>
-          <MediumText>If you’ve ever taken a bite from a ripe red apple, <br></br>
-          you’ll know the way this coffee tastes. </MediumText>
+          <MediumText>If you’ve ever taken a bite from a ripe red apple,
+            you’ll know the way this coffee tastes. </MediumText>
         </Detail>
 
-        <HorizontalBar src={HorizontalImage} alt="Horizontal Bar" />
+        <DesktopHorizontalBar src={HorizontalImage} alt="Horizontal Bar" />
+        <MobileHorizontalBar src={HorizontalImage} alt="Mobile Horizontal Bar" />
 
         <DetailsContainer>
           <Detail>
             <DetailTitle>TASTING NOTES</DetailTitle>
-            <TastingNotesIcon />
-            <DetailText>Red Apple, Lemongrass, <br></br>
-              Malt Chocolate</DetailText>
+            <DetailContent>
+              <TastingNotesIcon />
+              <DetailText>Red Apple, Lemongrass, <br></br>
+                Malt Chocolate</DetailText>
+            </DetailContent>
           </Detail>
 
-          <VerticalBar src={VerticalImage} alt="Vertical Bar" />
+          <DesktopVerticalBar src={VerticalImage} alt="Vertical Bar" />
+          <MobileHorizontalBar src={HorizontalImage} alt="Mobile Horizontal Bar" />
 
           <Detail>
             <DetailTitle>ROAST LEVEL</DetailTitle>
-            <RoastLevelIcon />
-            <DetailText>Medium</DetailText>
+            <DetailContent>
+              <RoastLevelIcon />
+              <DetailText>Medium</DetailText>
+            </DetailContent>
           </Detail>
 
-          <VerticalBar src={VerticalImage} alt="Vertical Bar" />
+          <DesktopVerticalBar src={VerticalImage} alt="Vertical Bar" />
+          <MobileHorizontalBar src={HorizontalImage} alt="Mobile Horizontal Bar" />
 
           <Detail>
             <DetailTitle>SIPPING STYLE</DetailTitle>
-            <SippingStyleIcon />
-            <DetailText>Great with milk/cream</DetailText>
+            <DetailContent>
+              <SippingStyleIcon />
+              <DetailText>Great with milk/cream</DetailText>
+            </DetailContent>
           </Detail>
 
-          <VerticalBar src={VerticalImage} alt="Vertical Bar" />
+          <DesktopVerticalBar src={VerticalImage} alt="Vertical Bar" />
+          <MobileHorizontalBar src={HorizontalImage} alt="Mobile Horizontal Bar" />
 
           <Detail>
             <DetailTitle>BREW METHOD</DetailTitle>
-            <BrewMethodIcon />
-            <DetailText>Brews a delicious cup <br></br> with a pour over</DetailText>
+            <DetailContent>
+              <BrewMethodIcon />
+              <DetailText>Brews a delicious cup <br></br> with a pour over</DetailText>
+            </DetailContent>
           </Detail>
         </DetailsContainer>
       </SpecsContainer>
